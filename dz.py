e521b002d25d407be8e871c268ea9bc4f33d5485
Monem <abdoeladawy4@gmail.com>
2024-03-28 22:18:49 +0200

    init commit

---
+++ dz.py
@@ -0,0 +1,414 @@
+#Static Folder Name
+folder_name = "welly" 
+
+dz_array = {
+        "public":{
+            "favicon":f"{folder_name}/images/favicon.png",
+            "description":"Welly is a clean-code, responsive Django Admin template that can be easily customized to fit the needs of various hospital, medical dashboard, health, doctor, and other businesses.",
+            "og_title":"Welly - Django Hospital Admin Dashboard Bootstrap Template",
+            "og_description":"Welly is a clean-code, responsive Django Admin template that can be easily customized to fit the needs of various hospital, medical dashboard, health, doctor, and other businesses.",
+            "og_image":"https://welly.dexignzone.com/django/social-image.png",
+            "title":"Welly - Django Hospital Admin Dashboard Bootstrap Template",
+        },
+        "global":{
+            "css":[
+                    f"{folder_name}/vendor/bootstrap-select/dist/css/bootstrap-select.min.css",
+                    f"{folder_name}/css/style.css"
+                ],
+
+            "js":{
+                "top":[
+                    f"{folder_name}/vendor/global/global.min.js",
+                    f"{folder_name}/vendor/bootstrap-select/dist/js/bootstrap-select.min.js",
+                ],
+                "bottom":[
+                    f"{folder_name}/js/custom.min.js",
+                    f"{folder_name}/js/deznav-init.js",
+                ]
+            },
+
+        },
+        "pagelevel":{
+            "welly":{#AppName
+                "welly_views":{
+                    "css":{
+                        "index": [
+							f"{folder_name}/vendor/chartist/css/chartist.min.css",
+							f"{folder_name}/vendor/bootstrap-datetimepicker/css/bootstrap-datetimepicker.min.css",	
+						],
+						"patient": [
+							f"{folder_name}/vendor/jqvmap/css/jqvmap.min.css",
+							f"{folder_name}/vendor/chartist/css/chartist.min.css",
+							f"{folder_name}/vendor/datatables/css/jquery.dataTables.min.css",
+						],
+						"patient_details": [
+							f"{folder_name}/vendor/jqvmap/css/jqvmap.min.css",
+							f"{folder_name}/vendor/chartist/css/chartist.min.css",
+							f"{folder_name}/vendor/datatables/css/jquery.dataTables.min.css",
+						],
+						"doctor": [
+							f"{folder_name}/vendor/jqvmap/css/jqvmap.min.css",
+							f"{folder_name}/vendor/chartist/css/chartist.min.css",
+							f"{folder_name}/vendor/datatables/css/jquery.dataTables.min.css",
+						],
+						"doctor_details": [
+							f"{folder_name}/vendor/jqvmap/css/jqvmap.min.css",
+							f"{folder_name}/vendor/chartist/css/chartist.min.css",
+							f"{folder_name}/vendor/bootstrap-datetimepicker/css/bootstrap-datetimepicker.min.css",	
+						],
+						"reviews": [
+							f"{folder_name}/vendor/jqvmap/css/jqvmap.min.css",
+							f"{folder_name}/vendor/chartist/css/chartist.min.css",
+							f"{folder_name}/vendor/datatables/css/jquery.dataTables.min.css",
+						],
+						"app_profile": [
+							f"{folder_name}/vendor/lightgallery/css/lightgallery.min.css",
+						],
+						"post_details": [
+							f"{folder_name}/vendor/lightgallery/css/lightgallery.min.css",
+						],
+						"app_calendar": [
+							f"{folder_name}/vendor/fullcalendar/css/main.min.css",
+						],
+						"chart_chartist": [
+							f"{folder_name}/vendor/chartist/css/chartist.min.css",
+						],
+						"chart_chartjs": [
+						],
+						"chart_flot": [
+						],
+						"chart_morris": [
+						],
+						"chart_peity": [
+						],
+						"chart_sparkline": [
+						],
+						"ecom_checkout": [
+						],
+						"ecom_customers": [
+						],
+						"ecom_invoice": [
+						],
+						"ecom_product_detail": [
+							f"{folder_name}/vendor/star-rating/star-rating-svg.css",
+						],
+						"ecom_product_grid": [
+						],
+						"ecom_product_list": [
+							f"{folder_name}/vendor/star-rating/star-rating-svg.css",
+						],
+						"ecom_product_order": [
+						],
+						"email_compose": [
+							f"{folder_name}/vendor/dropzone/dist/dropzone.css",
+						],
+						"email_inbox": [
+						],
+						"email_read": [
+						],
+						"form_editor": [
+						],
+						"form_element": [
+						],
+						"form_pickers": [
+							f"{folder_name}/vendor/bootstrap-daterangepicker/daterangepicker.css",
+							f"{folder_name}/vendor/clockpicker/css/bootstrap-clockpicker.min.css",
+							f"{folder_name}/vendor/jquery-asColorPicker/css/asColorPicker.min.css",
+							f"{folder_name}/vendor/bootstrap-material-datetimepicker/css/bootstrap-material-datetimepicker.css",
+							f"{folder_name}/vendor/pickadate/themes/default.css",
+							f"{folder_name}/vendor/pickadate/themes/default.date.css",
+						],
+						"form_validation": [
+						],
+						"form_wizard": [
+							f"{folder_name}/vendor/jquery-smartwizard/dist/css/smart_wizard.min.css",
+						],
+						"map_jqvmap": [
+							f"{folder_name}/vendor/jqvmap/css/jqvmap.min.css",
+						],
+						"table_bootstrap_basic": [
+						],
+						"table_datatable_basic": [
+							f"{folder_name}/vendor/datatables/css/jquery.dataTables.min.css",
+						],
+						"uc_lightgallery": [
+							f"{folder_name}/vendor/lightgallery/css/lightgallery.min.css",
+						],
+						"uc_nestable": [
+							f"{folder_name}/vendor/nestable2/css/jquery.nestable.min.css",
+						],
+						"uc_noui_slider": [
+							f"{folder_name}/vendor/nouislider/nouislider.min.css",
+						],
+						"uc_select2": [
+							f"{folder_name}/vendor/select2/css/select2.min.css",
+						],
+						"uc_sweetalert": [
+							f"{folder_name}/vendor/sweetalert2/dist/sweetalert2.min.css",
+						],
+						"uc_toastr": [
+							f"{folder_name}/vendor/toastr/css/toastr.min.css",
+						],
+						"ui_accordion": [
+						],
+						"ui_alert": [
+						],
+						"ui_badge": [
+						],
+						"ui_button": [
+						],
+						"ui_button_group": [
+						],
+						"ui_card": [
+						],
+						"ui_carousel": [
+						],
+						"ui_dropdown": [
+						],
+						"ui_grid": [
+						],
+						"ui_list_group": [
+						],
+						"ui_media_object": [
+						],
+						"ui_modal": [
+						],
+						"ui_pagination": [
+						],
+						"ui_popover": [
+						],
+						"ui_progressbar": [
+						],
+						"ui_tab": [
+						],
+						"ui_typography": [
+						],
+						"widget_basic": [
+							f"{folder_name}/vendor/chartist/css/chartist.min.css",
+						],
+						"page_empty": [
+						],
+                    },
+                    "js":{
+						"index": [
+							f"{folder_name}/vendor/chart-js/chart.bundle.min.js",
+							f"{folder_name}/vendor/bootstrap-datetimepicker/js/moment.js",
+							f"{folder_name}/vendor/bootstrap-datetimepicker/js/bootstrap-datetimepicker.min.js",
+							f"{folder_name}/vendor/peity/jquery.peity.min.js",
+							f"{folder_name}/vendor/apexchart/apexchart.js",
+							f"{folder_name}/js/dashboard/dashboard-1.js",
+						],
+						"patient": [
+							f"{folder_name}/vendor/chart-js/chart.bundle.min.js",
+							f"{folder_name}/vendor/datatables/js/jquery.dataTables.min.js",
+						],
+						"patient_details": [
+							f"{folder_name}/vendor/chart-js/chart.bundle.min.js",
+							f"{folder_name}/vendor/peity/jquery.peity.min.js",
+							f"{folder_name}/vendor/apexchart/apexchart.js",
+							f"{folder_name}/js/dashboard/patient-details.js",
+						],
+						"doctor": [
+							f"{folder_name}/vendor/chart-js/chart.bundle.min.js",
+							f"{folder_name}/vendor/datatables/js/jquery.dataTables.min.js",
+						],
+						"doctor_details": [
+							f"{folder_name}/vendor/chart-js/chart.bundle.min.js",
+							f"{folder_name}/vendor/bootstrap-datetimepicker/js/moment.js",
+							f"{folder_name}/vendor/bootstrap-datetimepicker/js/bootstrap-datetimepicker.min.js",
+							f"{folder_name}/vendor/peity/jquery.peity.min.js",
+							f"{folder_name}/vendor/apexchart/apexchart.js",
+							f"{folder_name}/js/dashboard/doctor-details.js",
+						],
+						"reviews": [
+							f"{folder_name}/vendor/chart-js/chart.bundle.min.js",
+						],
+						"app_calendar": [
+							f"{folder_name}/vendor/jqueryui/js/jquery-ui.min.js",
+							f"{folder_name}/vendor/moment/moment.min.js",
+							f"{folder_name}/vendor/fullcalendar/js/main.min.js",
+							f"{folder_name}/js/plugins-init/fullcalendar-init.js",
+						],
+						"app_profile": [
+							f"{folder_name}/vendor/lightgallery/js/lightgallery-all.min.js",	
+						],
+						"post_details": [
+							f"{folder_name}/vendor/lightgallery/js/lightgallery-all.min.js",
+						],
+						"chart_chartist": [
+							f"{folder_name}/vendor/chartist/js/chartist.min.js",
+							f"{folder_name}/vendor/chartist-plugin-tooltips/js/chartist-plugin-tooltip.min.js",
+							f"{folder_name}/js/plugins-init/chartist-init.js",
+						],
+						"chart_chartjs": [
+							f"{folder_name}/vendor/chart-js/chart.bundle.min.js",
+							f"{folder_name}/js/plugins-init/chartjs-init.js",
+						],
+						"chart_flot": [
+							f"{folder_name}/vendor/flot/jquery.flot.js",
+							f"{folder_name}/vendor/flot/jquery.flot.pie.js",
+							f"{folder_name}/vendor/flot/jquery.flot.resize.js",
+							f"{folder_name}/vendor/flot-spline/jquery.flot.spline.min.js",
+							f"{folder_name}/js/plugins-init/flot-init.js",
+						],
+						"chart_morris": [
+							f"{folder_name}/vendor/raphael/raphael.min.js",
+							f"{folder_name}/vendor/morris/morris.min.js",
+							f"{folder_name}/js/plugins-init/morris-init.js",
+						],
+						"chart_peity": [
+							f"{folder_name}/vendor/peity/jquery.peity.min.js",
+							f"{folder_name}/js/plugins-init/piety-init.js",
+						],
+						"chart_sparkline": [
+							f"{folder_name}/vendor/jquery-sparkline/jquery.sparkline.min.js",
+							f"{folder_name}/js/plugins-init/sparkline-init.js",
+						],
+						"ecom_checkout": [
+						],
+						"ecom_customers": [
+						],
+						"ecom_invoice": [
+						],
+						"ecom_product_detail": [
+							f"{folder_name}/vendor/star-rating/jquery.star-rating-svg.js",
+						],
+						"ecom_product_grid": [
+						],
+						"ecom_product_list": [
+							f"{folder_name}/vendor/star-rating/jquery.star-rating-svg.js",
+						],
+						"ecom_product_order": [
+						],
+						"email_compose": [
+							f"{folder_name}/vendor/dropzone/dist/dropzone.js",
+						],
+						"email_inbox": [
+						],
+						"email_read": [
+						],
+						"form_editor": [
+							f"{folder_name}/vendor/ckeditor/ckeditor.js",
+						],
+						"form_element": [
+						],
+						"form_pickers": [
+							f"{folder_name}/vendor/moment/moment.min.js",
+							f"{folder_name}/vendor/bootstrap-daterangepicker/daterangepicker.js",
+							f"{folder_name}/vendor/clockpicker/js/bootstrap-clockpicker.min.js",
+							f"{folder_name}/vendor/jquery-asColor/jquery-asColor.min.js",
+							f"{folder_name}/vendor/jquery-asGradient/jquery-asGradient.min.js",
+							f"{folder_name}/vendor/jquery-asColorPicker/js/jquery-asColorPicker.min.js",
+							f"{folder_name}/vendor/bootstrap-material-datetimepicker/js/bootstrap-material-datetimepicker.js",
+							f"{folder_name}/vendor/pickadate/picker.js",
+							f"{folder_name}/vendor/pickadate/picker.time.js",
+							f"{folder_name}/vendor/pickadate/picker.date.js",
+							f"{folder_name}/js/plugins-init/bs-daterange-picker-init.js",
+							f"{folder_name}/js/plugins-init/clock-picker-init.js",
+							f"{folder_name}/js/plugins-init/jquery-asColorPicker.init.js",
+							f"{folder_name}/js/plugins-init/material-date-picker-init.js",
+							f"{folder_name}/js/plugins-init/pickadate-init.js",
+						],
+						"form_validation": [
+							f"{folder_name}/vendor/jquery-validation/jquery.validate.min.js",
+							f"{folder_name}/js/plugins-init/jquery.validate-init.js",
+						],
+						"form_wizard": [
+							f"{folder_name}/vendor/jquery-steps/build/jquery.steps.min.js",
+							f"{folder_name}/vendor/jquery-validation/jquery.validate.min.js",
+							f"{folder_name}/js/plugins-init/jquery.validate-init.js",
+							f"{folder_name}/vendor/jquery-smartwizard/dist/js/jquery.smartWizard.js",
+						],
+						"map_jqvmap": [
+							f"{folder_name}/vendor/jqvmap/js/jquery.vmap.min.js",
+							f"{folder_name}/vendor/jqvmap/js/jquery.vmap.world.js",
+							f"{folder_name}/vendor/jqvmap/js/jquery.vmap.usa.js",
+							f"{folder_name}/js/plugins-init/jqvmap-init.js",
+						],
+						"table_bootstrap_basic": [
+						],
+						"table_datatable_basic": [
+							f"{folder_name}/vendor/datatables/js/jquery.dataTables.min.js",
+							f"{folder_name}/js/plugins-init/datatables.init.js",
+						],
+						"uc_lightgallery": [
+							f"{folder_name}/vendor/lightgallery/js/lightgallery-all.min.js",	
+						],
+						"uc_nestable": [
+							f"{folder_name}/vendor/nestable2/js/jquery.nestable.min.js",
+							f"{folder_name}/js/plugins-init/nestable-init.js",
+						],
+						"uc_noui_slider": [
+							f"{folder_name}/vendor/nouislider/nouislider.min.js",
+							f"{folder_name}/vendor/wnumb/wNumb.js",
+							f"{folder_name}/js/plugins-init/nouislider-init.js",
+						],
+						"uc_select2": [
+							f"{folder_name}/vendor/select2/js/select2.full.min.js",
+							f"{folder_name}/js/plugins-init/select2-init.js",
+						],
+						"uc_sweetalert": [
+							f"{folder_name}/vendor/sweetalert2/dist/sweetalert2.min.js",
+							f"{folder_name}/js/plugins-init/sweetalert.init.js",
+						],
+						"uc_toastr": [
+							f"{folder_name}/vendor/toastr/js/toastr.min.js",
+							f"{folder_name}/js/plugins-init/toastr-init.js",
+						],
+						"ui_accordion": [
+						],
+						"ui_alert": [
+						],
+						"ui_badge": [
+						],
+						"ui_button": [
+						],
+						"ui_button_group": [
+						],
+						"ui_card": [
+						],
+						"ui_carousel": [
+						],
+						"ui_dropdown": [
+						],
+						"ui_grid": [
+						],
+						"ui_list_group": [
+						],
+						"ui_media_object": [
+						],
+						"ui_modal": [
+						],
+						"ui_pagination": [
+						],
+						"ui_popover": [
+						],
+						"ui_progressbar": [
+						],
+						"ui_tab": [
+						],
+						"ui_typography": [
+						],
+						"widget_basic": [
+							f"{folder_name}/vendor/chartist/js/chartist.min.js",
+							f"{folder_name}/vendor/chartist-plugin-tooltips/js/chartist-plugin-tooltip.min.js",
+							f"{folder_name}/vendor/flot/jquery.flot.js",
+							f"{folder_name}/vendor/flot/jquery.flot.pie.js",
+							f"{folder_name}/vendor/flot/jquery.flot.resize.js",
+							f"{folder_name}/vendor/flot-spline/jquery.flot.spline.min.js",
+							f"{folder_name}/vendor/jquery-sparkline/jquery.sparkline.min.js",
+							f"{folder_name}/js/plugins-init/sparkline-init.js",
+							f"{folder_name}/vendor/peity/jquery.peity.min.js",
+							f"{folder_name}/js/plugins-init/piety-init.js",
+							f"{folder_name}/vendor/chart-js/chart.bundle.min.js",
+							f"{folder_name}/js/plugins-init/widgets-script-init.js",
+						],
+						"page_empty": [
+						],
+                    },
+                }
+            }
+        }
+
+
+}
---
+++ welly/welly_views.py
@@ -0,0 +1,533 @@
+from django.shortcuts import render
+
+
+def index(request):
+    context={
+        "page_title":"Dashboard"
+    }
+    return render(request,'welly/index.html',context)
+
+
+def patient(request):
+    context={
+        "page_title":"Patient"
+    }
+    return render(request,'welly/patient.html',context)
+
+
+def patient_details(request):
+    context={
+        "page_title":"Patient Detail"
+    }
+    return render(request,'welly/patient-details.html',context)
+
+
+def doctor(request):
+    context={
+        "page_title":"Doctor"
+    }
+    return render(request,'welly/doctor.html',context)
+
+
+def doctor_details(request):
+    context={
+        "page_title":"Doctor Details"
+    }
+    return render(request,'welly/doctor-details.html',context)
+
+
+def reviews(request):
+    context={
+        "page_title":"Reviews"
+    }
+    return render(request,'welly/reviews.html',context)
+
+
+
+def app_profile(request):
+    context={
+        "page_title":"App Profile"
+    }
+    return render(request,'welly/apps/app-profile.html',context)
+
+
+def post_details(request):
+    context={
+        "page_title":"Post Details"
+    }
+    return render(request,'welly/apps/post-details.html',context)
+
+
+def email_compose(request):
+    context={
+        "page_title":"Compose"
+    }
+    return render(request,'welly/apps/email/email-compose.html',context)
+
+
+def email_inbox(request):
+    context={
+        "page_title":"Inbox"
+    }
+    return render(request,'welly/apps/email/email-inbox.html',context)
+
+
+def email_read(request):
+    context={
+        "page_title":"Read"
+    }
+    return render(request,'welly/apps/email/email-read.html',context)
+
+
+def app_calendar(request):
+    context={
+        "page_title":"Calendar"
+    }
+    return render(request,'welly/apps/app-calendar.html',context)
+
+
+def ecom_product_grid(request):
+    context={
+        "page_title":"Product Grid"
+    }
+    return render(request,'welly/apps/shop/ecom-product-grid.html',context)
+
+
+def ecom_product_list(request):
+    context={
+        "page_title":"Product List"
+    }
+    return render(request,'welly/apps/shop/ecom-product-list.html',context)
+
+
+def ecom_product_detail(request):
+    context={
+        "page_title":"Product Detail"
+    }
+    return render(request,'welly/apps/shop/ecom-product-detail.html',context)
+
+
+def ecom_product_order(request):
+    context={
+        "page_title":"Product Order"
+    }
+    return render(request,'welly/apps/shop/ecom-product-order.html',context)
+
+
+def ecom_checkout(request):
+    context={
+        "page_title":"Checkout"
+    }
+    return render(request,'welly/apps/shop/ecom-checkout.html',context)
+
+
+def ecom_invoice(request):
+    context={
+        "page_title":"Invoice"
+    }
+    return render(request,'welly/apps/shop/ecom-invoice.html',context)
+
+
+def ecom_customers(request):
+    context={
+        "page_title":"Customers"
+    }
+    return render(request,'welly/apps/shop/ecom-customers.html',context)
+
+
+def flat_icons(request):
+    context={
+        "page_title":"Flat Icons"
+    }
+    return render(request,'welly/flat-icons.html',context)
+
+
+def svg_icons(request):
+    context={
+        "page_title":"Svg Icons"
+    }
+    return render(request,'welly/svg-icons.html',context)
+
+
+def feather_icons(request):
+    context={
+        "page_title":"Feather Icons"
+    }
+    return render(request,'welly/feather-icons.html',context)
+
+
+def content(request):
+    context={
+        "page_title":"Content"
+    }
+    return render(request,'welly/cms/content.html',context)
+
+
+def add_content(request):
+    context={
+        "page_title":"Add Content"
+    }
+    return render(request,'welly/cms/add-content.html',context)
+
+
+def menu(request):
+    context={
+        "page_title":"Menu"
+    }
+    return render(request,'welly/cms/menu.html',context)
+
+
+def email_template(request):
+    context={
+        "page_title":"Email Template"
+    }
+    return render(request,'welly/cms/email-template.html',context)
+
+
+def add_email(request):
+    context={
+        "page_title":"Add Email"
+    }
+    return render(request,'welly/cms/add-email.html',context)
+
+
+def blog(request):
+    context={
+        "page_title":"Blog"
+    }
+    return render(request,'welly/cms/blog.html',context)
+
+
+def add_blog(request):
+    context={
+        "page_title":"Add Blog"
+    }
+    return render(request,'welly/cms/add-blog.html',context)
+
+
+def blog_category(request):
+    context={
+        "page_title":"Blog Category"
+    }
+    return render(request,'welly/cms/blog-category.html',context)
+
+
+def chart_flot(request):
+    context={
+        "page_title":"Chart Flot"
+    }
+    return render(request,'welly/charts/chart-flot.html',context)
+
+
+def chart_morris(request):
+    context={
+        "page_title":"Chart Morris"
+    }
+    return render(request,'welly/charts/chart-morris.html',context)
+
+
+def chart_chartjs(request):
+    context={
+        "page_title":"Chart Chartjs"
+    }
+    return render(request,'welly/charts/chart-chartjs.html',context)
+
+
+def chart_chartist(request):
+    context={
+        "page_title":"Chart Chartist"
+    }
+    return render(request,'welly/charts/chart-chartist.html',context)
+
+
+def chart_sparkline(request):
+    context={
+        "page_title":"Chart Sparkline"
+    }
+    return render(request,'welly/charts/chart-sparkline.html',context)
+
+
+def chart_peity(request):
+    context={
+        "page_title":"Chart Peity"
+    }
+    return render(request,'welly/charts/chart-peity.html',context)
+
+
+
+def ui_accordion(request):
+    context={
+        "page_title":"Accordion"
+    }
+    return render(request,'welly/bootstrap/ui-accordion.html',context)
+
+
+def ui_alert(request):
+    context={
+        "page_title":"Alert"
+    }
+    return render(request,'welly/bootstrap/ui-alert.html',context)
+
+
+def ui_badge(request):
+    context={
+        "page_title":"Badge"
+    }
+    return render(request,'welly/bootstrap/ui-badge.html',context)
+
+
+def ui_button(request):
+    context={
+        "page_title":"Button"
+    }
+    return render(request,'welly/bootstrap/ui-button.html',context)
+
+
+def ui_modal(request):
+    context={
+        "page_title":"Modal"
+    }
+    return render(request,'welly/bootstrap/ui-modal.html',context)
+
+
+def ui_button_group(request):
+    context={
+        "page_title":"Button Group"
+    }
+    return render(request,'welly/bootstrap/ui-button-group.html',context)
+
+
+def ui_list_group(request):
+    context={
+        "page_title":"List Group"
+    }
+    return render(request,'welly/bootstrap/ui-list-group.html',context)
+
+
+def ui_media_object(request):
+    context={
+        "page_title":"Media Object"
+    }
+    return render(request,'welly/bootstrap/ui-media-object.html',context)
+
+
+def ui_card(request):
+    context={
+        "page_title":"Card"
+    }
+    return render(request,'welly/bootstrap/ui-card.html',context)
+
+
+def ui_carousel(request):
+    context={
+        "page_title":"Carousel"
+    }
+    return render(request,'welly/bootstrap/ui-carousel.html',context)
+
+
+def ui_dropdown(request):
+    context={
+        "page_title":"Dropdown"
+    }
+    return render(request,'welly/bootstrap/ui-dropdown.html',context)
+
+
+def ui_popover(request):
+    context={
+        "page_title":"Popover"
+    }
+    return render(request,'welly/bootstrap/ui-popover.html',context)
+
+
+def ui_progressbar(request):
+    context={
+        "page_title":"Progressbar"
+    }
+    return render(request,'welly/bootstrap/ui-progressbar.html',context)
+
+
+def ui_tab(request):
+    context={
+        "page_title":"Tab"
+    }
+    return render(request,'welly/bootstrap/ui-tab.html',context)
+
+
+def ui_typography(request):
+    context={
+        "page_title":"Typography"
+    }
+    return render(request,'welly/bootstrap/ui-typography.html',context)
+
+
+def ui_pagination(request):
+    context={
+        "page_title":"Pagination"
+    }
+    return render(request,'welly/bootstrap/ui-pagination.html',context)
+
+
+def ui_grid(request):
+    context={
+        "page_title":"Grid"
+    }
+    return render(request,'welly/bootstrap/ui-grid.html',context)
+
+
+
+
+def uc_select2(request):
+    context={
+        "page_title":"Select"
+    }
+    return render(request,'welly/plugins/uc-select2.html',context)
+
+
+def uc_nestable(request):
+    context={
+        "page_title":"Nestable"
+    }
+    return render(request,'welly/plugins/uc-nestable.html',context)
+
+
+def uc_noui_slider(request):
+    context={
+        "page_title":"UI Slider"
+    }
+    return render(request,'welly/plugins/uc-noui-slider.html',context)
+
+
+def uc_sweetalert(request):
+    context={
+        "page_title":"Sweet Alert"
+    }
+    return render(request,'welly/plugins/uc-sweetalert.html',context)
+
+
+def uc_toastr(request):
+    context={
+        "page_title":"Toastr"
+    }
+    return render(request,'welly/plugins/uc-toastr.html',context)
+
+
+def map_jqvmap(request):
+    context={
+        "page_title":"Jqvmap"
+    }
+    return render(request,'welly/plugins/map-jqvmap.html',context)
+
+
+def uc_lightgallery(request):
+    context={
+        "page_title":"LightGallery"
+    }
+    return render(request,'welly/plugins/uc-lightgallery.html',context)
+
+
+def widget_basic(request):
+    context={
+        "page_title":"Widget"
+    }
+    return render(request,'welly/widget-basic.html',context)
+
+
+def form_element(request):
+    context={
+        "page_title":"Form Element"
+    }
+    return render(request,'welly/forms/form-element.html',context)
+
+
+def form_wizard(request):
+    context={
+        "page_title":"Form Wizard"
+    }
+    return render(request,'welly/forms/form-wizard.html',context)
+
+
+def form_editor(request):
+    context={
+        "page_title":"CkEditor"
+    }
+    return render(request,'welly/forms/form-editor.html',context)
+
+
+def form_pickers(request):
+    context={
+        "page_title":"Pickers"
+    }
+    return render(request,'welly/forms/form-pickers.html',context)
+
+
+def form_validation(request):
+    context={
+        "page_title":"Form Validation"
+    }
+    return render(request,'welly/forms/form-validation.html',context)
+
+
+def table_bootstrap_basic(request):
+    context={
+        "page_title":"Table Bootstrap"
+    }
+    return render(request,'welly/table/table-bootstrap-basic.html',context)
+
+
+def table_datatable_basic(request):
+    context={
+        "page_title":"Table Datatable"
+    }
+    return render(request,'welly/table/table-datatable-basic.html',context)
+
+
+
+
+
+
+def page_register(request):
+    return render(request,'welly/pages/page-register.html')
+
+def page_login(request):
+    return render(request,'welly/pages/page-login.html')
+
+def page_forgot_password(request):
+    return render(request,'welly/pages/page-forgot-password.html')
+
+def page_lock_screen(request):
+    return render(request,'welly/pages/page-lock-screen.html')
+
+def page_empty(request):
+    context={
+        "page_title":"Empty Page"
+    }
+    return render(request,'welly/pages/page-empty.html',context)
+
+def page_error_400(request):
+    return render(request,'400.html')
+    
+def page_error_403(request):
+    return render(request,'403.html')
+
+def page_error_404(request):
+    return render(request,'404.html')
+
+def page_error_500(request):
+    return render(request,'500.html')
+
+def page_error_503(request):
+    return render(request,'503.html')
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
